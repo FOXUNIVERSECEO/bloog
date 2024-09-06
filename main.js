e3539d14d24fb200eaa7762c9b9e9346d9bacc15
hariharan c <hari19bcs068@gmail.com>
2024-09-06 16:41:48 +0530

    fivth commit

--- main.js
+++ main.js
@@ -1,92 +1,26 @@
-var swiper = new Swiper(".swiper", {
-    autoplay: {
-        delay: 5000,
-    },
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    mousewheel: true,
-    speed: 1000,
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 1,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
-    on: {
-        slideChange: function () {
-            var currentSlide = this.slides[this.activeIndex];
-            gsap.fromTo(currentSlide.querySelector("img"),
-                {
-                    scale: 1.2,
-                }, {
-                scale: 1,
-                duration: 5
-            })
-            gsap.fromTo(currentSlide.querySelector(".t-bold"),
-                {
-                    y: 100,
-                    opacity: 0
-                },
-                {
-                    y: 0,
-                    opacity: 1,
-                    duration: 1
-                })
-            gsap.fromTo(currentSlide.querySelector(".t-thin span"),
-                {
-                    x: -100,
-                    opacity: 0
-                },
-                {
-                    x: 0,
-                    opacity: 1,
-                    duration: 1
-                })
-            gsap.fromTo(currentSlide.querySelector(".t-bg"),
-                {
-                    width: "0",
-                },
-                {
-                    width: "112%",
-                    duration: 0.5,
-                    delay: 0.5
-                })
-            gsap.fromTo(currentSlide.querySelector(".quote"),
-                {
-                    y: 200,
-                    opacity: 0
-                },
-                {
-                    y: 0,
-                    opacity: 1,
-                    duration: 1,
-                    delay: 0.5
-                })
-            gsap.fromTo(currentSlide.querySelectorAll(".counter"),
-                {
-                    y: 200,
-                    opacity: 0
-                },
-                {
-                    y: 0,
-                    opacity: 1,
-                    duration: 1,
-                    delay: 0.8
-                })
-        },
-    },
-});
-// footer start
-// Initialization for ES Users
-import { Ripple, initMDB } from "mdb-ui-kit";
+// nav background
+let header = document.querySelector("header");
 
-initMDB({ Ripple });
-// footer end
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0)
+})
+
+//Filter
+$(document).ready(function () {
+    $(".filter-item").click(function () {
+        const value = $(this).attr("data-filter");
+        if (value == "all"){
+            $(".post-box").show("1000")
+        } else{
+            $(".post-box")
+                .not("." + value)
+                .hide(1000);
+            $(".post-box")
+            .filter("." + value)
+            .show("1000")
+        }
+    });
+    $(".filter-item").click(function () {
+        $(this).addClass("active-filter").siblings().removeClass("active-filter")
+    });
+});
